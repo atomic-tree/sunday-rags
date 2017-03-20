1f8fc95024e177d1ef70570b4543c841bd8d5e42
Andrei Pavlyuk <devdice@outlook.com>
2017-03-20 10:07:08 +0200

    add list

--- src/list.jsx
+++ src/list.jsx
@@ -6,13 +6,12 @@ import data from './data.json';
 
 const List = ({ id, title }) => {
   let actors = data.map((actor, i) => {
-    return <Profile key={i} name={actor.name} />
+    return <Profile key={i} name={actor.name} />;
   });
 
   return (
     <div className='list'>
-            <h2 id={  id}>
-              { title}</h2>
+      <h2 id={id}>{title}</h2>
       {actors}
     </div>
   );
