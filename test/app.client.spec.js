c4b76d361ff5981e3f9f373da510965fa7cd5dac
Andrei Pavlyuk <devdice@outlook.com>
2017-03-28 13:03:16 +0300

    istanbul.js для покрытия тестов

--- test/app.client.spec.js
+++ test/app.client.spec.js
@@ -1,3 +1,9 @@
+let expect = require('expect');
+
 describe('App', () => {
-  it('should be able to run tests');
+  it('should be able to run tests', () => {
+    let word = 'hello world';
+
+    expect(word).toBe('hello world');
+  });
 });
