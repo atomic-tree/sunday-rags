8ed507498fd93eea6db305118d3a699913f0f3c3
Andrei Pavlyuk <devdice@outlook.com>
2017-03-30 07:46:54 +0300

    добавил иконки в разметку

--- src/components/time.counter.jsx
+++ src/components/time.counter.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Terrain from 'react-icons/lib/md/terrain';
+import SnowFlake from 'react-icons/lib/ti/weather-snow';
+import Calendar from 'react-icons/lib/fa/calendar';
+
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%');
@@ -13,14 +17,17 @@ const TimeCounter = ({ total, powder, backcountry, goal }) => {
     <div className="time-counter">
       <div className="total-days">
         <span>{total}</span>
+        <Calendar />
         <span>days</span>
       </div>
       <div className="powder-days">
         <span>{powder}</span>
+        <SnowFlake />
         <span>days</span>
       </div>
       <div className="backcountry-days">
         <span>{backcountry}</span>
+        <Terrain />
         <span>days</span>
       </div>
       <div>
