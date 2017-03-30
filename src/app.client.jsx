0605acef249dd47baf67390f917e10bcc1f73c91
Andrei Pavlyuk <devdice@outlook.com>
2017-03-30 07:18:08 +0300

    добавлен коунтер времени-дней

--- src/app.client.jsx
+++ src/app.client.jsx
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import Layout from './layout';
 import InfoPanel from './components/info.panel';
 import List from './components/list';
+import TimeCounter from './components/time.counter';
 import 'stylesheets/globals';
 
 import bookmarks from './data/bookmarks';
@@ -17,7 +18,12 @@ const App = () => {
       <InfoPanel introduction={introduction} tagline={tagline} />
       <nav className='board-container'>
         <List id='my-profiles' title='My Profiles' data={bookmarks} />
+        <TimeCounter total={30}
+          powder={20}
+          backcountry={10}
+          goal={100} />
       </nav>
+
     </Layout>
   );
 };
--- src/components/time.counter.jsx
+++ src/components/time.counter.jsx
@@ -1,3 +1,35 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%');
+};
 
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total / goal);
+};
+
+const TimeCounter = ({ total, powder, backcountry, goal }) => {
+  return (
+    <div className="time-counter">
+      <div className="total-days">
+        <span>{total}</span>
+        <span>days</span>
+      </div>
+      <div className="powder-days">
+        <span>{powder}</span>
+        <span>days</span>
+      </div>
+      <div className="backcountry-days">
+        <span>{backcountry}</span>
+        <span>days</span>
+      </div>
+      <div>
+        <span>
+          {calcGoalProgress(total, goal)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TimeCounter;
